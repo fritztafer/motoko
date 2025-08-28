804905315b66c014c278f6ad4810e0cd128dbf76
fritztafer <fritzistheman@gmail.com>
2025-08-28 15:14:15 -0700

    Replaced imports of discord with discord classes

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,4 +1,4 @@
-import discord
+from discord import Member, User
 from discord.ext import commands
 from motoko import Motoko
 import utils.decorators as decorators
@@ -12,7 +12,7 @@ class Mod(commands.Cog):
     @commands.hybrid_command(name='ban', description='ban user (delete messages)')
     @commands.has_permissions(ban_members=True)
     @decorators.sync()
-    async def ban(self, ctx: commands.Context[Motoko], user: discord.Member, *, reason: str | None):
+    async def ban(self, ctx: commands.Context[Motoko], user: Member, *, reason: str | None):
         await user.ban(reason=reason, delete_message_days=7)
         await ctx.reply(f'{user.mention} was **banned** from **{ctx.guild}**')
 
@@ -20,7 +20,7 @@ class Mod(commands.Cog):
     @commands.hybrid_command(name='unban', description='unban user')
     @commands.has_permissions(ban_members=True)
     @decorators.sync()
-    async def unban(self, ctx: commands.Context[Motoko], user: discord.User, *, reason: str | None):
+    async def unban(self, ctx: commands.Context[Motoko], user: User, *, reason: str | None):
         if ctx.guild:
             await ctx.guild.unban(user, reason=reason)
             await ctx.reply(f'{user.mention} was **unbanned** from **{ctx.guild}**')
@@ -29,7 +29,7 @@ class Mod(commands.Cog):
     @commands.hybrid_command(name='softban', description='softban user (kick & delete messages)')
     @commands.has_permissions(ban_members=True)
     @decorators.sync()
-    async def softban(self, ctx: commands.Context[Motoko], user: discord.Member, *, reason: str | None):
+    async def softban(self, ctx: commands.Context[Motoko], user: Member, *, reason: str | None):
         await user.ban(reason=f'[softban] {reason}', delete_message_days=7)
         await user.unban(reason='softban reversal')
         await ctx.reply(f'{user.mention} was **kicked** from **{ctx.guild}**')
@@ -38,7 +38,7 @@ class Mod(commands.Cog):
     @commands.hybrid_command(name='kick', description='kick user (does not delete messages)')
     @commands.has_permissions(ban_members=True)
     @decorators.sync()
-    async def kick(self, ctx: commands.Context[Motoko], user: discord.Member, *, reason: str | None):
+    async def kick(self, ctx: commands.Context[Motoko], user: Member, *, reason: str | None):
         await user.kick(reason=reason)
         await ctx.reply(f'{user.mention} was **kicked** from **{ctx.guild}**')
 
@@ -46,7 +46,7 @@ class Mod(commands.Cog):
     @commands.hybrid_command(name='mute', description='mute user (timeout)')
     @commands.has_permissions(ban_members=True)
     @decorators.sync()
-    async def mute(self, ctx: commands.Context[Motoko], user: discord.Member, minutes: int, *, reason: str | None):
+    async def mute(self, ctx: commands.Context[Motoko], user: Member, minutes: int, *, reason: str | None):
         duration = timedelta(minutes=minutes)
         await user.timeout(duration, reason=reason)
         await ctx.reply(f'{user.mention} was **muted** for **{minutes} minutes**')
@@ -55,7 +55,7 @@ class Mod(commands.Cog):
     @commands.hybrid_command(name='unmute', description='unmute user')
     @commands.has_permissions(ban_members=True)
     @decorators.sync()
-    async def unmute(self, ctx: commands.Context[Motoko], user: discord.Member, *, reason: str | None):
+    async def unmute(self, ctx: commands.Context[Motoko], user: Member, *, reason: str | None):
         await user.timeout(None, reason=reason)
         await ctx.reply(f'{user.mention} was **unmuted**')
 
